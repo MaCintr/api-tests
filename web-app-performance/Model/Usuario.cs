48d49121129d31e56b4192c0180e3d9bb91b0433
matpazikas <matheuspazikas@gmail.com>
2024-09-19 22:17:47 -0300

    Api Funcionando

---
+++ web-app-performance/Model/Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace web_app_performance.Model
+{
+    public class Usuario
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+    }
+}
